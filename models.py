fa734068fa69b33ec55fbe3689b5102c7eb05f5a
raghavagarwal <raghavagarwal3618@gmail.com>
2025-03-26 14:53:13 +0530

    Add database, authentication, and logging features

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from database import Base
